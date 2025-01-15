7fe1e8811968d8c5f66f1975cb9ac7ec5a52054a
kadin <kadinpadams@gmail.com>
2025-01-14 20:36:48 -0700

    assignment 2

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,41 @@
+function productCardTemplate(product)
+{
+    return `<li class="product-card">
+  <a href="product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Image}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
+}
+
+export default class ProductListing
+{
+    constructor(category, dataSource, listElement)
+    {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init()
+    {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        // If the list is empty, show a message
+        if (list.length === 0) {
+          this.listElement.innerHTML = 'No products available.';
+          return;
+        }
+    
+        // Generate the HTML for each product and insert it into the DOM
+        const htmlStrings = list.map(productCardTemplate); // Create HTML for each product
+        this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join('')); // Insert into the list
+      }
+  }
